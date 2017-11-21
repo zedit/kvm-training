07143fffbdce1b1a0eb56e19f568c4dd075b85da
Dmitriy Yevtushenko <dmitriy.yevtushenko@gmail.com>
2017-11-21 15:48:00 +0200

    adding different terms to the script

--- script.sh
+++ script.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+def_link="https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img"
+def_rsa_file="/home/dmitry/.ssh/id_rsa.pub"
 VM_NAME="${1}"
-VM_PASSWORD="${2}"
-SSH_PUBLIC_KEY="${3}"
-IMG_LINK="${4}"
+SSH_PUBLIC_KEY="${2:-$def_rsa_file}"
+IMG_LINK="${3:-$def_link}"
+DIR_NAME="/var/lib/libvirt/images/${VM_NAME}"
+CHECK_SUM="99e73c2c09cad6a681b2d372c37f2e11"
 
 function downloadImg {
-   mkdir ${VM_NAME}
-   wget -O /var/lib/libvirt/images/${VM_NAME}/${VM_NAME}.img "${1}"
+   if [ ! -e "$DIR_NAME" ]
+   then
+      mkdir ${DIR_NAME} 
+   fi
+   if [ ! -f "${DIR_NAME}/${VM_NAME}".img ]
+   then
+      wget -O ${DIR_NAME}/${VM_NAME}.img "${1}"
+   else 
+      local existing_file_check_sum=$(md5sum ${DIR_NAME}/${VM_NAME}.img)
+      if [ "${CHECK_SUM}" == "${existing_file_check_sum}" ]
+      then
+         echo wget -O ${DIR_NAME}/${VM_NAME}.img "${1}"
+      fi
+   fi
 }
 
 function createUserdata {
-   local key_file=${2}
+   local key_file=${1}
    mkdir /tmp/${VM_NAME}
    cp templates/user-data.template /tmp/${VM_NAME}/user-data
-   sed -i "s/sed_change_password/${1}/" /tmp/${VM_NAME}/user-data
    PUBLIC_KEY_CONTENT=$(cat ${key_file})
    sed -i "s#sed_change_public_key#${PUBLIC_KEY_CONTENT}#" /tmp/${VM_NAME}/user-data
-   mkdir -p /var/lib/libvirt/images/${VM_NAME}
-   cloud-localds /var/lib/libvirt/images/${VM_NAME}/user-data.img /tmp/${VM_NAME}/user-data
+   mkdir -p ${DIR_NAME}
+   cloud-localds ${DIR_NAME}/user-data.img /tmp/${VM_NAME}/user-data
    rm -rf /tmp/${VM_NAME}/  
 }
 
@@ -27,10 +41,10 @@ function creatVm {
                 --ram 512 \
                 --vcpus=1 \
                 --noautoconsole \
-                --cdrom=/var/lib/libvirt/images/${VM_NAME}/user-data.img \
-                --disk path=/var/lib/libvirt/images/${VM_NAME}/${VM_NAME}.img,format=qcow2
+                --cdrom=${DIR_NAME}/user-data.img \
+                --disk path=${DIR_NAME}/${VM_NAME}.img,format=qcow2
 }
 
 downloadImg $IMG_LINK 
-createUserdata $VM_PASSWORD $SSH_PUBLIC_KEY
+createUserdata $SSH_PUBLIC_KEY
 creatVm 
